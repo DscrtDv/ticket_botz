cd2a333207ecb7fa89e059a82f4de6d14caef3aa
Tim Ddev <ti.discretedev@gmail.com>
2023-10-07 12:44:49 +0200

    capt

--- src/puppet.js
+++ src/puppet.js
@@ -5,6 +5,7 @@ const mailer    = require('./mailer.js');
 const utils       = require('./utils.js');
 
 var isAvailable = false;
+var captcha     = false;
 
 const loadCookie = async (page) => {
     try {
@@ -24,13 +25,39 @@ const getTicket = async (page) => {
         console.log("[/] Getting ticket..");
         await page.click('.e1asqgj30');
         const buttonText = 'Buy ticket';
-        await page.waitForNavigation();
         const [buttonElement] = await page.$x(`//button[contains(text(), "${buttonText}")]`);
         await buttonElement.click();
         console.log("[+] Tickets Saved !");
     } catch(err) {
         console.error("[x] Error while fetching the ticket: ", err);
     }
+}
+
+
+//<p class="title" id="rtitle" style="visibility: visible; display: block; color: rgb(251, 188, 5);">Human verification in process...</p>
+const checkForCaptcha = async (page) => {
+    try {
+        const elText = 'Human verification in process...';
+        const [el] = await page.$x(`//p[contains(text(), "${elText}")]`);
+        if (el)
+            captcha = true;
+        else
+            captcha = false;
+    } catch (err) {
+        console.error("[x] Error while checking for Human verification: ", err);
+    }
+}
+
+function captchaTimeout(browser){
+    const time = 3*60*1000;
+    browser.close();
+    console.log("[?] Browser closed due to Human verification");
+    console.log("[?] Timer set to 3 minutes.");
+    setTimeout(function(){
+        console.log("[?] Retrying..");
+        captcha = false;
+        setPuppeteer();
+    }, time);
 }
 
 async function setPuppeteer()
@@ -43,13 +70,13 @@ async function setPuppeteer()
     console.log("[?] Info: Started at: " + start_info);
     puppeteer.use(stealth());
     const browser = await puppeteer.launch({
-        headless: true,
-        slowMo: 250,
+        headless: false,
+        slowMo: 50,
     });
     const page = (await browser.pages())[0];
     
     await loadCookie(page);
-    await page.goto("https://www.ticketswap.com/event/ares/1c80c5f7-c1f3-46a6-baba-d4bc99f4190e");
+    await page.goto("https://www.ticketswap.com/event/live-from-earth-ade-2023/regular-tickets/6de1657f-d678-4e6c-b9f8-12a6ed522516/3058095");
     while (!isAvailable)
     {
         var ticket_button = await page.$('.e1asqgj30');
@@ -58,9 +85,15 @@ async function setPuppeteer()
             isAvailable = true;
             await getTicket(page);
         }
-        else {
-            console.log("[?] No ticket found, reloading...");
-            await page.waitForTimeout(15000);
+        else {  
+            await checkForCaptcha(page);
+            console.log("[?] Captcha: " + captcha);
+            if (captcha == true){
+                captchaTimeout(browser);
+                return ;
+            }
+            console.log("[/] No ticket found, reloading...");
+            await page.waitForTimeout(30000);
             await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
         }
     }
